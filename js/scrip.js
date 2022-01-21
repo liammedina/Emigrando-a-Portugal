cff462d1cefbad0bfea28b0863e8c2aa3c14a1d8
Lia M. Medina <54250767+Niugler@users.noreply.github.com>
2022-01-21 20:59:16 -0300

    Iniciando GIT en el proyecto

---
+++ js/scrip.js
@@ -0,0 +1,9 @@
+document.getElementById("iconMenu").addEventListener("click", mostrar_header);
+
+function mostrar_header(){
+    document.getElementById("MenuEmergente").classList.toggle('headerAShow');
+}
+
+function mensaje(){
+    alert("Hola mundo");
+}
